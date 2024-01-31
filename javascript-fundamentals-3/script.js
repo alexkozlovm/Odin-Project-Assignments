15aa65803fb064b1a11caf7143c5f9695e6c5dc7
Alexander Kozlov <alex-k@akozlov.lan>
2024-01-31 18:07:13 -0500

    Finished js-f-3

--- javascript-fundamentals-3/script.js
+++ javascript-fundamentals-3/script.js
@@ -2,3 +2,15 @@ function add7(num) {
     return 7 + num;
 }
 console.log(add7(2))
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(3, 2));
+function capitalize(word) {
+    return `${word.slice(0).charAt(0).toUpperCase()}${word.slice(1)}`;
+}
+console.log(capitalize("hello"))
+function lastLetter(word) {
+    return word.slice(word.length - 1);
+}
+console.log(lastLetter("test"));
